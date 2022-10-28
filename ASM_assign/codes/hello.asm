2a0c4eeb5a96200587febd2331f94d2daadc1a5d
kumarg9999 <111555500+kumarg9999@users.noreply.github.com>
2022-10-28 11:29:29 +0530

    Add files via upload

---
+++ ASM_assign/codes/hello.asm
@@ -0,0 +1,12 @@
+;using assembly language for turning LED on
+
+
+.include "/home/dell/m328Pdef.inc"
+
+
+  ldi r16,0b00100000
+  out DDRB,r16
+  ldi r17,0b00100000
+  out PortB,r17
+Start:
+  rjmp Start
